733acb8452ab4d1cca7aad51c993f538a5ca0d20
Lindblad1999 <emil.lindblad.christensen@gmail.com>
2023-11-15 12:12:44 +0100

    Added relay control

--- Embedded/src/main.cpp
+++ Embedded/src/main.cpp
@@ -3,13 +3,19 @@
 
 void setup() {
   Serial.begin(115200);
-  analogReadResolution(12);  
+  analogReadResolution(12);
+  pinMode(GPIO_NUM_22, OUTPUT);
 }
 
 void loop() {
-  int voltageSensorRaw = analogRead(GPIO_NUM_36);
+  int voltageSensorRaw = analogRead(GPIO_NUM_33);
   Serial.println(voltageSensorRaw);
   int currentSensorRaw = analogRead(GPIO_NUM_39);
   Serial.println(currentSensorRaw);
+
+  digitalWrite(GPIO_NUM_22, HIGH);
   delay(1000);
+  digitalWrite(GPIO_NUM_22, LOW);
+  delay(1000);
+
 }
