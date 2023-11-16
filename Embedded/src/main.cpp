d4c5e0cd53bc2b75932111f9874a19fef5130343
Lindblad1999 <emil.lindblad.christensen@gmail.com>
2023-11-16 13:44:45 +0100

    Setting up WiFi

--- Embedded/src/main.cpp
+++ Embedded/src/main.cpp
@@ -1,21 +1,28 @@
 #include <Arduino.h>
-
+#include <WiFi.h>
+#include "config.h"
 
 void setup() {
-  Serial.begin(115200);
-  analogReadResolution(12);
+  Serial.begin(115200); // Open serial connection (To be removed)
   pinMode(GPIO_NUM_22, OUTPUT);
+  digitalWrite(GPIO_NUM_22, HIGH); // Activate relay to close circuit as default.
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+  }
+  Serial.println("Connected to Wifi");
+  
 }
 
 void loop() {
-  int voltageSensorRaw = analogRead(GPIO_NUM_33);
+  int voltageSensorRaw = analogRead(GPIO_NUM_36);
   Serial.println(voltageSensorRaw);
   int currentSensorRaw = analogRead(GPIO_NUM_39);
   Serial.println(currentSensorRaw);
 
-  digitalWrite(GPIO_NUM_22, HIGH);
-  delay(1000);
-  digitalWrite(GPIO_NUM_22, LOW);
   delay(1000);
 
 }
