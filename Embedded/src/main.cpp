b2353b6740db00298805d630df9165a03671a020
Lindblad1999 <emil.lindblad.christensen@gmail.com>
2023-11-25 23:40:16 +0100

    Voltage and current ADC convertions

--- Embedded/src/main.cpp
+++ Embedded/src/main.cpp
@@ -17,12 +17,32 @@ void setup() {
   
 }
 
+float calculate_voltage(int rawData) {
+  // Divide the raw ADC reading with the ADC resoulution (12-bit)
+  // and multiply with Vref
+  float voltage = (float)rawData / 4096 * 3.3;
+
+  // The sensor outputs in a 1:5 ratio, so there must be multiplied with 5
+  voltage = voltage * 5;
+
+  return voltage;
+}
+
+float calculate_current(int rawData) {
+  float current = (float)rawData - 2500;
+  current = current / 4096 * 3.3;
+  
+  return current;
+}
+
 void loop() {
   int voltageSensorRaw = analogRead(GPIO_NUM_36);
-  Serial.println(voltageSensorRaw);
+  Serial.println(calculate_voltage(voltageSensorRaw));
+
   int currentSensorRaw = analogRead(GPIO_NUM_39);
-  Serial.println(currentSensorRaw);
+  Serial.println(calculate_current(currentSensorRaw));
 
   delay(1000);
 
 }
+
