f991e4d561c9e674a12584e5fedb8b062b963dbf
Lindblad1999 <emil.lindblad.christensen@gmail.com>
2023-11-25 22:54:54 +0100

    current post test, and voltage closest test

--- API/tests/test.py
+++ API/tests/test.py
@@ -11,9 +11,14 @@ class APITestCase(unittest.TestCase):
         self.client = self.app.test_client()
         db.create_all()
 
-    def test_test(self):
-        self.assertEqual(1, 1)
-
     def test_voltage_post(self):
         response = self.client.post('/voltage', json={'meas': 9.5, 'device_id': 1})
         self.assertEqual(response.status_code, 201)
+    
+    def test_current_post(self):
+        response = self.client.post('/current', json={'meas': 1.5, 'device_id': 1})
+        self.assertEqual(response.status_code, 201)
+    
+    def test_voltage_get_closest(self):
+        response = self.client.get('/voltage/closest?timestamp=2023-11-21T16:01:00')
+        self.assertEqual(response.status_code, 200)
