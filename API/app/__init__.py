402313d4306fe43178c2c19311f8253d71bf890d
Lindblad1999 <emil.lindblad.christensen@gmail.com>
2023-11-28 17:55:53 +0100

    Fix CORS isssue

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__) # Create instance of Flask application
+    CORS(app)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///energy_meter.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
