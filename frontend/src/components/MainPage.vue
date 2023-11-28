79fdab917ff10e5e675e6659d7da16b5030f0840
Lindblad1999 <emil.lindblad.christensen@gmail.com>
2023-11-28 18:06:31 +0100

    Added custom input field for the API IP address

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,27 +1,42 @@
 <template>
     <div>
-        <h1>MAIN PAGE</h1>
-        <p style="margin-left: 50%;">
+        <h1 style="margin-left: 40%;">Smart Home Energy Meter</h1>
+        <p>
             {{ info }}
         </p>
+    </div>
+    <div>
+        <form @submit.prevent="setApiAddress">
+            <label>API IP Address:</label>
+            <input type="text" v-model="apiInput">
+            <button type="submit">Set</button>
+        </form>
     </div>
 </template>
 
 
 <script>
-import TEST_API from '@/constants'
+// import TEST_API from '@/constants'
 
 import axios from 'axios'
 export default {
     data() {
         return {
+            apiInput: '',
+            apiAddress: '',
             info: null
         };
     },
-    mounted() {
-        axios
-            .get(TEST_API)
-            .then(response => (this.info = response.status))
+    methods:{
+        setApiAddress() {
+            this.apiAddress = this.apiInput;
+            axios.get(`http://${this.apiAddress}`)
+                .then(response => this.info = response.data)
+        },
+        // getVoltage() {
+        //     axios.get('${this.apiAddress}/voltage')
+        //         .then(response => this.info = response)
+        // }
     }
 }
 </script>
