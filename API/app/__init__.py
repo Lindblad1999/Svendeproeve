15aa14c636d58e693cb29ec512deb0733b66f929
Lindblad1999 <emil.lindblad.christensen@gmail.com>
2023-11-29 02:01:36 +0100

    create db migrations

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
 db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__) # Create instance of Flask application
     CORS(app)
 
+    migrate = Migrate(app, db)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-'
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
