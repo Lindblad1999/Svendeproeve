6b8c2e38d3ad50438c8cb38d2f83aac9bc57fd1b
Lindblad1999 <emil.lindblad.christensen@gmail.com>
2023-11-29 06:31:56 +0100

    Calculate and display current power consumption

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
         <h1>Smart Home Energy Meter</h1>
-        <p>
-            {{ voltage }}
-        </p>
+    </div>
+    <div id="main-device">
+        <div id="power-consumption-current">
+            <h2 id="power-consumption">Current power consumption</h2>
+            <p id="latest-wattage">{{ latestWattage }} Watts</p>
+        </div>
     </div>
 </template>
 
@@ -16,19 +19,33 @@ export default {
         return {
             api_url: API_URL,
             dateTime: null,
-            voltage: 0,
+            latestVoltage: 0,
+            latestCurrent: 0,
+            latestWattage: 0,
             value: 0
         };
     },
     mounted() {
-        setInterval(this.getVoltage, 1000);
+        setInterval(this.getLatestWattage, 1000);
     },
     methods:{
-        getVoltage() {
+        getLatestVoltage() {
             this.dateTime = new Date().toISOString().replace(".", ":").slice(0, -5);
             axios.get(`${API_URL}/voltage/closest?timestamp=${this.dateTime}`)
-                .then(response => this.voltage = response.data.meas)
-            }
+                .then(response => this.latestVoltage = response.data.meas)
+        },
+        getLatestCurrent() {
+            this.dateTime = new Date().toISOString().replace(".", ":").slice(0, -5);
+            axios.get(`${API_URL}/current/closest?timestamp=${this.dateTime}`)
+                .then(response => this.latestCurrent = Math.abs(response.data.meas))
+        },
+        getLatestWattage() {
+            this.getLatestVoltage();
+            this.getLatestCurrent();
+            console.log(`volt: ${this.latestVoltage}`)
+            console.log(`current: ${this.latestCurrent}`)
+            this.latestWattage = (this.latestVoltage * this.latestCurrent).toFixed(5);
+        }
     }
 }
 </script>
@@ -37,7 +54,28 @@ export default {
     h1 {
         text-align: center;
     }
-    p {
+
+    #main-device {
+        background-color: darkgrey;
+        height: 50vh;
+        width: 50vw;
+        border-radius: 50px;
+        border: 5px dotted;
+        margin-left: 25%;
+    }
+
+    #latest-wattage {
         text-align: center;
+    }
+
+    #power-consumption {
+        text-align: center;
+    }
+
+    #power-consumption-current {
+        background-color: gray;
+        width: 70%;
+        margin: 0 auto;
+        border-radius: 25px;
     }
 </style>
